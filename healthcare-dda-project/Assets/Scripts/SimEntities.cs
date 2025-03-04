bfb2c413225eab95df09388ebfbd3dc93aa3752d
SamGomes <sam.s.gomes@sapo.pt>
2025-03-04 20:17:41 +0000

    tested some simple VAs to check learning.

--- healthcare-dda-project/Assets/Scripts/SimEntities.cs
+++ healthcare-dda-project/Assets/Scripts/SimEntities.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Google.Protobuf.WellKnownTypes;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
@@ -22,7 +23,13 @@ namespace SimEntities
         //sets the improveRate to a random value between 0.4 and 0.7
         private float ImprovRate { get; } = Random.Range(0.5f,1.0f);
 
-
+        private float NormalizeFromCSV(int lvlTrIndex, string valueCSVAttr)
+        {
+            float value = Convert.ToSingle(m_Measures[lvlTrIndex][valueCSVAttr]); // cast to float did not work for some reason
+            float min = Convert.ToSingle(m_Measures[lvlTrIndex]["min."+valueCSVAttr]);
+            float max = Convert.ToSingle(m_Measures[lvlTrIndex]["max."+valueCSVAttr]);
+            return (value - min) / (max - min);
+        }
 
         public PatientWrapper(DDAAgent ddaAgentObject)
         {
@@ -37,10 +44,15 @@ namespace SimEntities
             if (game.CurrLvl > 0) //transition 00 does not make sense, so do nothing to condition
             {
                 int lvlTrIndex = (game.PrevLvl * game.NumLvls + game.CurrLvl) - 1; //transition 00 does not make sense
-                float RSQScore = Convert.ToSingle(m_Measures[lvlTrIndex]["RSQ_mrbluesky_score"]);
-                float normalizedRSQScore = RSQScore / 50.0f;
-                // Debug.Log(game.PrevLvl + ";" + game.CurrLvl + "->" + RSQScore);
-                Condition += normalizedRSQScore; // cast to float did not work for some reason
+                float condInc = 0.0f;
+                condInc += NormalizeFromCSV(lvlTrIndex,"averageTimeRight.second_played_lvl");
+                condInc += NormalizeFromCSV(lvlTrIndex,"averageTimeLeft.second_played_lvl");
+                condInc += 1.0f - NormalizeFromCSV(lvlTrIndex,"stressLevel_delta.second_played_lvl.mrbluesky");
+                condInc += 1.0f - NormalizeFromCSV(lvlTrIndex,"heartRate_delta.second_played_lvl.mrbluesky");
+                condInc += NormalizeFromCSV(lvlTrIndex,"average_displacement1_mrbluesky.second_played_lvl");
+                condInc += NormalizeFromCSV(lvlTrIndex,"average_displacement2_mrbluesky.second_played_lvl");
+                condInc += NormalizeFromCSV(lvlTrIndex,"RSQ_mrbluesky_score_delta");
+                Condition += condInc/7.0f; 
             }
             PlayedLvls++;
             
